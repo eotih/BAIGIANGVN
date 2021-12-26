cdeca296e3fb3a1d085a3a3cd6bcd92d952eb5ec
Le Trong Hieu <eotihvn@gmail.com>
2021-12-26 13:16:59 +0700

    init

---
+++ server/BAIGIANGVN/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BAIGIANGVN.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ server/BAIGIANGVN/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BAIGIANGVN.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
